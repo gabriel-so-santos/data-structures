ae2392f0a224058176d0f93762f13246b33140e1
Gabriel <santos.gabriel@aluno.ufca.edu.br>
2026-04-04 20:33:36 -0300

    style(list): shorten macro parameter ListName -> Name

--- inc/libds/list.h
+++ inc/libds/list.h
@@ -10,14 +10,14 @@
 #include <libds/core.h>
 #include <libds/err.h>
 
-#define DS_DEFINE_LIST(Type, ListName, destructor_func)                         \
+#define DS_DEFINE_LIST(Type, Name, destructor_func)                             \
                                                                                 \
-    typedef TypeWrapper ds_##ListName##_t;                                      \
+    typedef TypeWrapper ds_##Name##_t;                                          \
                                                                                 \
-    static inline ds_##ListName##_t                                             \
-    ds_##ListName##_create(void)                                                \
+    static inline ds_##Name##_t                                                 \
+    ds_##Name##_create(void)                                                    \
     {                                                                           \
-        ds_##ListName##_t list = {                                              \
+        ds_##Name##_t list = {                                                  \
             .value_size = sizeof(Type),                                         \
             .destructor = (destructor_func),                                    \
             .nodechain_ptr = ds__nc_alloc(),                                    \
@@ -26,7 +26,7 @@
     }                                                                           \
                                                                                 \
     static inline ds_err_t                                                      \
-    ds_##ListName##_destroy(ds_##ListName##_t *list)                            \
+    ds_##Name##_destroy(ds_##Name##_t *list)                                    \
     {                                                                           \
         DS__CHECK_ERR(                                                          \
             ds__nc_free(&list->nodechain_ptr, list->destructor)                 \
@@ -34,19 +34,19 @@
     }                                                                           \
                                                                                 \
     static inline size_t                                                        \
-    ds_##ListName##_length_of(ds_##ListName##_t list)                           \
+    ds_##Name##_length_of(ds_##ListName##_t list)                               \
     {                                                                           \
         return ds__nc_length_of(list.nodechain_ptr);                            \
     }                                                                           \
                                                                                 \
     static inline bool                                                          \
-    ds_##ListName##_is_empty(ds_##ListName##_t list)                            \
+    ds_##Name##_is_empty(ds_##Name##_t list)                                    \
     {                                                                           \
         return ds__nc_is_empty(list.nodechain_ptr);                             \
     }                                                                           \
                                                                                 \
     static inline ds_err_t                                                      \
-    ds_##ListName##_first(ds_##ListName##_t list, Type *output_ptr)             \
+    ds_##Name##_first(ds_##Name##_t list, Type *output_ptr)                     \
     {                                                                           \
         DS__CHECK_ERR(                                                          \
             ds__nc_get_front(list.nodechain_ptr, output_ptr, list.value_size)   \
@@ -54,7 +54,7 @@
     }                                                                           \
                                                                                 \
     static inline ds_err_t                                                      \
-    ds_##ListName##_last(ds_##ListName##_t list, Type *output_ptr)              \
+    ds_##Name##_last(ds_##Name##_t list, Type *output_ptr)                      \
     {                                                                           \
         DS__CHECK_ERR(                                                          \
             ds__nc_get_back(list.nodechain_ptr, output_ptr, list.value_size)    \
@@ -62,15 +62,20 @@
     }                                                                           \
                                                                                 \
     static inline ds_err_t                                                      \
-    ds_##ListName##_get_at(ds_##ListName##_t list, Type *output_ptr, long long index)\
+    ds_##Name##_get_at(ds_##ListName##_t list, Type *output_ptr, long long index)\
     {                                                                           \
         DS__CHECK_ERR(                                                          \
-            ds__nc_get_at(list.nodechain_ptr, output_ptr, list.value_size, index)\
+            ds__nc_get_at(                                                      \
+                list.nodechain_ptr,                                             \
+                output_ptr,                                                     \
+                list.value_size,                                                \
+                index                                                           \
+            )                                                                   \
         );                                                                      \
     }                                                                           \
                                                                                 \
     static inline ds_err_t                                                      \
-    ds_##ListName##_append(ds_##ListName##_t list, Type value)                  \
+    ds_##Name##_append(ds_##Name##_t list, Type value)                          \
     {                                                                           \
         DS__CHECK_ERR(                                                          \
             ds__nc_push_back(list.nodechain_ptr, &value, list.value_size)       \
@@ -78,7 +83,7 @@
     }                                                                           \
                                                                                 \
     static inline ds_err_t                                                      \
-    ds_##ListName##_prepend(ds_##ListName##_t list, Type value)                 \
+    ds_##Name##_prepend(ds_##Name##_t list, Type value)                         \
     {                                                                           \
         DS__CHECK_ERR(                                                          \
             ds__nc_push_front(list.nodechain_ptr, &value, list.value_size)      \
@@ -86,7 +91,7 @@
     }                                                                           \
                                                                                 \
     static inline ds_err_t                                                      \
-    ds_##ListName##_insert(ds_##ListName##_t list, Type value, long long index) \
+    ds_##Name##_insert(ds_##Name##_t list, Type value, long long index)         \
     {                                                                           \
         DS__CHECK_ERR(                                                          \
             ds__nc_push_at(list.nodechain_ptr, &value, list.value_size, index)  \
@@ -94,14 +99,14 @@
     }                                                                           \
                                                                                 \
     static inline ds_err_t                                                      \
-    ds_##ListName##_remove_first(ds_##ListName##_t list)                        \
+    ds_##Name##_remove_first(ds_##ListName##_t list)                            \
     {                                                                           \
         DS__CHECK_ERR(                                                          \
             ds__pop_front(list.nodechain_ptr, list.destructor)                  \
         );                                                                      \
     }                                                                           \
     static inline ds_err_t                                                      \
-    ds_##ListName##_remove_last(ds_##ListName##_t list)                         \
+    ds_##Name##_remove_last(ds_##ListName##_t list)                             \
     {                                                                           \
         DS__CHECK_ERR(                                                          \
             ds__pop_back(list.nodechain_ptr, list.destructor)                   \
